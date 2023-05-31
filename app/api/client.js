8b8c543e201c325ec3cab6701e6813aa1cc4f19e
kamalafidele <kamalafizzet45@gmail.com>
2023-05-31 08:44:44 +0200

    added the URL for hosted backend server

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from 'apisauce';
 
 const apiClient = create({
-    baseURL: 'http://192.168.8.168:7500/api',
+    baseURL: 'https://supa-menu-backend.onrender.com/api',
     timeout: 10000,
 });
 
